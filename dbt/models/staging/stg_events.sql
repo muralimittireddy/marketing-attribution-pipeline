f7cd4a4d483f82c630895ae9bdf2202a1693ad59
murali <muralimittireddy1234@gmail.com>
2025-10-19 13:45:42 +0530

    made event_id unique

--- dbt/models/staging/stg_events.sql
+++ dbt/models/staging/stg_events.sql
@@ -9,7 +9,7 @@ SELECT
     MAX(IF(param.key = 'value', CAST(param.value.double_value AS FLOAT64), NULL)) AS purchase_value,
     MAX(IF(param.key = 'currency', param.value.string_value, NULL)) AS currency,
     CASE WHEN event_name = 'purchase' THEN TRUE ELSE FALSE END AS is_conversion,
-    CONCAT(user_pseudo_id, '_', event_timestamp, '_', event_bundle_sequence_id) AS event_id
+    CONCAT(user_pseudo_id, '_', event_timestamp, '_', event_name) AS event_id
 FROM
     `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
 CROSS JOIN
@@ -18,5 +18,4 @@ CROSS JOIN
 GROUP BY
     user_pseudo_id,
     event_timestamp,
-    event_name,
-    event_bundle_sequence_id
+    event_name
